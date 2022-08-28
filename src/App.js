64fb105dbee337b54387c693ce76d4f7f35b6c8b
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2022-08-28 16:00:16 +0900

    add email state

--- src/App.js
+++ src/App.js
@@ -3,15 +3,30 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [email, setEmail] = useState("");
 
-  const onClick = () => {
+  const onIncrease = () => {
     setCount(count + 1);
+  };
+
+  const onDecrease = () => {
+    setCount(count - 1);
+  };
+
+  const updateEmail = (event) => {
+    const value = event.target.value;
+    setEmail(value);
   };
 
   return (
     <div>
       <h1>Count : {count}</h1>
-      <button onClick={onClick}>Click here for increasing Count!</button>
+      <button onClick={onIncrease}>Increasing</button>
+      <button onClick={onDecrease}>Decreasing</button>
+      <hr />
+      <div>
+        <input onChange={updateEmail} placeholder="Email" value={email} />
+      </div>
     </div>
   );
 }
