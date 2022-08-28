1ad82604ed81bea9c591645e9bea396fe34d9fdf
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2022-08-29 00:26:21 +0900

    add useTabs hook

--- src/App.js
+++ src/App.js
@@ -1,36 +1,55 @@
 import { useState } from "react";
 import "./App.css";
 
-const useInput = (initialValue, validator) => {
-  const [value, setValue] = useState(initialValue);
-  const onChange = (event) => {
-    const value = event.target.value;
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Section 1",
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Section 2",
+  },
+];
+// 'Content' will be like this
 
-    let willUpdate = true;
+// content[0] = Section 1
+// content[1] = Section 2
 
-    if (typeof validator === "function") {
-      willUpdate = validator(value);
-    }
+const useTabs = (initialTab, allTabs) => {
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  if (!allTabs || !Array.isArray(allTabs)) {
+    // if there is no allTabs , OR if it is not Array
+    // ** check it first, because it can be cause Error.
 
-    if (willUpdate) {
-      setValue(value);
-    }
+    return;
+    // if it has no Array NOR allTabs, return nothing.
+  }
+
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex,
   };
-  return { value, onChange };
 };
+// defaultTab (= currentItem) is '0'
 
 function App() {
-  const maxLen = (value) => !value.includes("@");
-
-  const name = useInput("Ms. ", maxLen);
-
+  const { currentItem, changeItem } = useTabs(0, content);
+  // useTabs returns currentItem.
+  // you should bring - useTabs(page Index, page content).
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <input placeholder="Name" {...name} />
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
     </div>
+    // you should bring currentItem's content below the Tab change button.
+
+    // each Section have a Button
+    // as same as pagination, you can switch to the page that you'd like to see.
   );
 }
-// class component use 'this'
 
 export default App;
+// class component use 'this'.
