90ef95cbcc72da5a1c8aeddde07cc560706a79b4
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2022-08-29 21:52:44 +0900

    add useEffect practice code

---
+++ src/prac-useEffect-hook.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const sayHello = () => console.log("Hello");
+  useEffect(() => {
+    sayHello();
+  }, []);
+  // when dependency is [] - it works only one time
+  // when dependency is [number] - it works only "number" value change.
+
+  const [number, setNumber] = useState(0);
+  const [aNumber, setAnumber] = useState(0);
+
+  return (
+    <div className="App">
+      <div>Hi</div>
+      <button onClick={() => setNumber(number + 1)}>{number}</button>
+      <button onClick={() => setAnumber(aNumber + 1)}>{aNumber}</button>
+    </div>
+  );
+}
+
+export default App;
