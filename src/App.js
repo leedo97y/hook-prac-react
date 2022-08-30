76e90bd23aef95b5400b3cbd0261f9daa4dc11ed
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2022-08-30 13:49:37 +0900

    add useClick by using useRef, useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef } from "react";
 import "./App.css";
 
-const useTitle = (initialTitle) => {
-  const [title, setTitle] = useState(initialTitle);
-  const updateTitle = () => {
-    const htmlTitle = document.querySelector("title");
-    htmlTitle.innerText = title;
-  };
-  useEffect(updateTitle, [title]);
-  return setTitle;
+const useClick = (onClick) => {
+  const element = useRef();
+  useEffect(() => {
+    if (typeof onClick !== "function") {
+      return;
+    }
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    };
+  }, [onClick]);
+  return typeof onClick !== "function" ? undefined : element;
 };
-// when dependency change, updateTitle works
 
 function App() {
-  const titleUpdater = useTitle("Loading...");
-  setTimeout(() => titleUpdater("Home"), 5000);
+  const sayHello = () => console.log("Say Hello");
+  const title = useClick(sayHello);
   return (
     <div className="App">
-      <div>Hi</div>
+      <h1 ref={title}>Hi</h1>
     </div>
   );
 }
