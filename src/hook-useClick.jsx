8fad079165a3507a6f9b9cf6133d2dabac607181
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2022-08-30 13:50:50 +0900

    learn useClick again

---
+++ src/hook-useClick.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useRef } from "react";
+import "./App.css";
+
+const useClick = (onClick) => {
+  const element = useRef();
+  useEffect(() => {
+    if (typeof onClick !== "function") {
+      return;
+    }
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    };
+  }, [onClick]);
+  return typeof onClick !== "function" ? undefined : element;
+};
+
+function App() {
+  const sayHello = () => console.log("Say Hello");
+  const title = useClick(sayHello);
+  return (
+    <div className="App">
+      <h1 ref={title}>Hi</h1>
+    </div>
+  );
+}
+
+export default App;
